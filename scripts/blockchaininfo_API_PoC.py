afb64059592412015c0bf414e2ac4f5b911609fe
heiseApple <44158204+heiseApple@users.noreply.github.com>
2023-10-20 12:01:47 +0200

    Added get_wallet method in blockchaininfo_API_PoC.py

--- scripts/blockchaininfo_API_PoC.py
+++ scripts/blockchaininfo_API_PoC.py
@@ -1,19 +1,44 @@
-import pprint
+from pprint import pprint
 import requests
 
-tx = f"0ED06D5B56F6AD8501FD336F7C78C9B66763201B2F152424404AA8D12787D2B7"
-res = requests.get(f"https://blockchain.info/rawtx/{tx}")
-red_dict = res.json()
 
-inputs = [i for i in red_dict.get("inputs")]
-for i in inputs:
-    satoshi = i.get("prev_out").get("value")
+def get_wallet(wallet_hash):
+    url = f"https://blockchain.info/rawaddr/{wallet_hash}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to retrieve data for block with hash {wallet_hash}")
+        return None
+    
+def get_transaction(tx_hash):
+    url = f"https://blockchain.info/rawtx/{tx_hash}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to retrieve data for block with hash {block_hash}")
+        return None
+
+
+tx_hash = f"0ED06D5B56F6AD8501FD336F7C78C9B66763201B2F152424404AA8D12787D2B7"
+tx_data = get_transaction(tx_hash)
+
+for i in tx_data.get("inputs"):
+    input_data = i.get("prev_out")
+    
+    satoshi = input_data.get("value")
     btc = float(satoshi / 10**8)
-    pprint.pprint(f"{btc}₿")
+    pprint(f"Input: {btc}₿")
+    
+    wallet_hash = input_data.get("addr")
+    wallet_data = get_wallet(wallet_hash)
+    pprint(f'Input balance: {wallet_data.get("final_balance")}₿')
 
 
-edges_count = len(red_dict.get("inputs")) == 1 and len(red_dict.get("out")) == 2
-
+edges_count = len(tx_data.get("inputs")) == 1 and len(tx_data.get("out")) == 2
 
 if all([edges_count]):
     print("Ok")

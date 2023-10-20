d2f8383cc0b5adb011c735d5b365fd76f31ec8b7
heiseApple <44158204+heiseApple@users.noreply.github.com>
2023-10-20 17:43:02 +0200

    Update extract_stats_from_txs.py

--- scripts/extract_stats_from_txs.py
+++ scripts/extract_stats_from_txs.py
@@ -2,6 +2,7 @@ import json
 import numpy as np
 
 deltas = []
+ratios = []
 
 with open('lazy_peel_chain_txs.json') as f:
     lazy_peel_chain_txs = json.load(f)
@@ -11,9 +12,17 @@ for tx in lazy_peel_chain_txs:
     if len(outs) != 2:
         raise Exception('TX must have 2 outs')
     
-    deltas.append(abs(outs[0]['amount'] - outs[1]['amount'])/10**8)
+    out0 = outs[0]['amount'] / 10**8
+    out1 = outs[1]['amount'] / 10**8
+    
+    deltas.append(abs(out0 - out1)) 
+    
+    max_out = max(out0, out1)
+    ratios.append(max_out/(out0 + out1))
     
 deltas = np.array(deltas)
+ratios = np.array(ratios)
 print(deltas.mean(), deltas.std())
+print(ratios.mean(), ratios.std())
 
-print(deltas)
+print(ratios)

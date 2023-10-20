3dbf55e8bc4e3f05bc2e5798474ffac6ebdacd69
tsumarios <mraciti96@gmail.com>
2023-10-20 12:20:34 +0200

    Refactoring

--- scripts/blockchaininfo_API_PoC.py
+++ scripts/blockchaininfo_API_PoC.py
@@ -2,43 +2,62 @@ from pprint import pprint
 import requests
 
 
-def get_wallet(wallet_hash):
-    url = f"https://blockchain.info/rawaddr/{wallet_hash}"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Failed to retrieve data for block with hash {wallet_hash}")
-        return None
-    
-def get_transaction(tx_hash):
-    url = f"https://blockchain.info/rawtx/{tx_hash}"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Failed to retrieve data for block with hash {block_hash}")
-        return None
+class BlockchainDataAPI:
+    @staticmethod
+    def get_block(block_hash):
+        url = f"https://blockchain.info/rawblock/{block_hash}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to retrieve data for block with hash {block_hash}")
+            return None
+
+    @staticmethod
+    def get_address(addr_hash):
+        url = f"https://blockchain.info/rawaddr/{addr_hash}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to retrieve data for block with hash {addr_hash}")
+            return None
+
+    @staticmethod
+    def get_transaction(tx_hash):
+        url = f"https://blockchain.info/rawtx/{tx_hash}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to retrieve data for transaction with hash {tx_hash}")
+            return None
 
 
-tx_hash = f"0ED06D5B56F6AD8501FD336F7C78C9B66763201B2F152424404AA8D12787D2B7"
-tx_data = get_transaction(tx_hash)
+def main():
+    api = BlockchainDataAPI()
+    tx_hash = "0ed06d5b56f6ad8501fd336f7c78c9b66763201b2f152424404aa8d12787d2b7"
+    tx_data = api.get_transaction(tx_hash)
 
-for i in tx_data.get("inputs"):
-    input_data = i.get("prev_out")
-    
-    satoshi = input_data.get("value")
-    btc = float(satoshi / 10**8)
-    pprint(f"Input: {btc}₿")
-    
-    wallet_hash = input_data.get("addr")
-    wallet_data = get_wallet(wallet_hash)
-    pprint(f'Input balance: {wallet_data.get("final_balance")}₿')
+    for i in tx_data.get("inputs"):
+        input_data = i.get("prev_out")
+
+        satoshi = input_data.get("value")
+        btc = float(satoshi / 10**8)
+        pprint(f"Input: {btc}₿")
+
+        addr_hash = input_data.get("addr")
+        addr_data = api.get_address(addr_hash)
+        pprint(f'Input balance: {addr_data.get("final_balance")}₿')
+
+    edges_count = len(tx_data.get("inputs")) == 1 and len(tx_data.get("out")) == 2
+
+    if all([edges_count]):
+        print("Ok")
 
 
-edges_count = len(tx_data.get("inputs")) == 1 and len(tx_data.get("out")) == 2
-
-if all([edges_count]):
-    print("Ok")
+if __name__ == "__main__":
+    main()

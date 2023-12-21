1f600bd432edddd353161413910067970a923eda
tsumarios <mraciti96@gmail.com>
2023-12-21 13:05:30 +0100

    Add atom types and checks in expressions

--- src/interpreter.py
+++ src/interpreter.py
@@ -12,9 +12,12 @@ with open("grammar.lark") as f:
 # Test queries
 test_queries = [
     "From Transaction 7a51a014f6bd3ccad3a403a99ad525f1aff310fbffe904bada56440d4abeba7f Check Transaction.hash = HEX 1231231a0714f6bd3ccad3a403a99ad525f1aff310fbffe904bada56440d4abeba7f",
+    "From Transaction 7a51a014f6bd3ccad3a403a99ad525f1aff310fbffe904bada56440d4abeba7f Check Transaction.relayed_by = IP 192.168.1.1",
+    "From Transaction 7a51a014f6bd3ccad3a403a99ad525f1aff310fbffe904bada56440d4abeba7f Check Transaction.double_spend = False",
     "From Transaction 7a51a014f6bd3ccad3a403a99ad525f1aff310fbffe904bada56440d4abeba7f Check Transaction.size = 210",
-    "From Address 7a51a014f6bd3ccad3a403a99ad525f1aff310fbffe904bada56440d4abeba7f Check Address.final_balance = 1000",
+    "From Address 7a51a014f6bd3ccad3a403a99ad525f1aff310fbffe904bada56440d4abeba7f Check Address.hash = HEX 32423ad",
     "From Transaction 7a51a014f6bd3ccad3a403a99ad525f1aff310fbffe904bada56440d4abeba7f Check (Transaction.size > 210) and (Transaction.size < 300)",
+    "From Address 7a51a014f6bd3ccad3a403a99ad525f1aff310fbffe904bada56440d4abeba7f Check (Xtrans Transaction.lock_time > 30) and (Faddr 5 Address.hash = HEX 7a51a014)",
 ]
 for tq in test_queries:
     parsed_query = lark_parser.parse(tq)
